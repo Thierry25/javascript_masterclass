bb9b34844a734db19288bb6834d708f95c365d1d
Thierry <marcelinthierry@gmail.com>
2023-01-30 11:22:38 -0500

    Add solution to challenge regarding map

--- challenges/10-challenge.js
+++ challenges/10-challenge.js
@@ -128,3 +128,44 @@ for (const scorer of game.scored) {
 }
 
 console.log(scored);
+
+// NEW-SECTION
+
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+// Task 1
+// Create an array 'events' of the different game events that happened (no duplicates)
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// Task 2
+// After the game has finished, it was found that the yellow card from minute 64 was unfair.
+// So remove this event from the game events log.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// Task 3
+// Compute and log the following string to the console :"An event happened,on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+const gameLength = 90;
+const avg = Math.ceil(gameLength / gameEvents.size);
+console.log(`An event happened, on average, every ${avg} minutes`);
+
+// Task 4
+// Loop over 'gameEvents' and log each element to the console,marking whether it's in the first half or second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17: ⚽   GOAL
+
+for (const [minute, event] of gameEvents) {
+  const half = minute <= 45 ? "[FIRST HALF] " : "[SECOND HALF] ";
+  console.log(`${half} ${event}`);
+}
